160b5aa073cb034c3dff652604f05a91ef4dca3a
Komine Ryuichi <systena@PCK-135-089-no-MacBook-Pro.local>
2016-08-22 13:04:05 +0900

    Git Test

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by PCK-135-089 on 2016/08/22.
+//  Copyright © 2016年 PCK-135-089. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by PCK-135-089 on 2016/08/22.
+//  Copyright © 2016年 PCK-135-089. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
