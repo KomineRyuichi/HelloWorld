ddf6a049716b13e9c3c013201d45fe90c64ceac6
Komine Ryuichi <systena@PCK-135-089-no-MacBook-Pro.local>
2016-08-22 13:16:32 +0900

    subのコミット

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     UILabel *label = [[UILabel alloc] init];
-    label.text = @"Hello 世界!";
+    label.text = @"こんにちは 世界!";
     label.center = self.view.center;
     [label sizeToFit];
     [self.view addSubview:label];
